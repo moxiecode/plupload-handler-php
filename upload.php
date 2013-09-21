c26e69866a19417d8aa3e3b5deb4536233d9a29f
Davit Barbakadze <jayarjo@gmail.com>
2013-09-21 11:29:35 +0400

    Adapt example.

--- upload.php
+++ upload.php
@@ -9,7 +9,13 @@ if (!PluploadHandler::handle(array(
 	'target_dir' => 'uploads/',
 	'allow_extensions' => 'jpg,jpeg,png'
 ))) {
-	die(json_encode(array('OK' => 0, 'error' => PluploadHandler::get_error())));
+	die(json_encode(array(
+		'OK' => 0, 
+		'error' => array(
+			'code' => PluploadHandler::get_error_code(),
+			'message' => PluploadHandler::get_error_message()
+		)
+	)));
 } else {
 	die(json_encode(array('OK' => 1)));
 }
