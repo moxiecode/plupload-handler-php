08323428fd47eb3c20162c7e765f76832d4a021b
Davit Barbakadze <jayarjo@gmail.com>
2016-11-21 00:09:36 +0400

    Return result along with OK.

--- upload.php
+++ upload.php
@@ -3,16 +3,18 @@
 require_once("PluploadHandler.php");
 
 $ph = new PluploadHandler(array(
-	'cleanup' => false,
-	'target_dir' => 'uploads/',
+	'target_dir' => '../uploads/',
 	'allow_extensions' => 'jpg,jpeg,png'
 ));
 
 $ph->send_nocache_headers();
 $ph->send_cors_headers();
 
-if ($ph->handle_upload()) {
-	die(json_encode(array('OK' => 1)));
+if ($result = $ph->handle_upload()) {
+	die(json_encode(array(
+		'OK' => 1,
+		'info' => $result
+	)));
 } else {
 	die(json_encode(array(
 		'OK' => 0,
